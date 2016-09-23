a18eea0a60d749ee92e03b61cc1523893faa6678
mpjunaid <mpjunaid1996@gmail.com>
2016-09-23 22:53:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName',function (req,res){
-    var articleName = rec.params.articleName;
+    var articleName = req.params.articleName;
     res.send(create(articles[articleName]));
 });
 
