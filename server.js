c1b54a154bb2ffd1bffe19decf6cd4974f2b6e56
mpjunaid <mpjunaid1996@gmail.com>
2016-09-30 14:23:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 /* var articles ={
  articleone : {
                     title:'Article one |mpjunaid',
